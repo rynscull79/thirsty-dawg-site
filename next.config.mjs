f9ab718728d1c6ad34fd1a39ea32c0daba5483de
rynscull79 <tuckerandharper@gmail.com>
2025-06-13 14:14:02 -0500

    update next.config for security

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,21 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   output: 'export',
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // apply to all routes
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self'; script-src 'self' https://connect.facebook.net; img-src 'self' data: https:;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
