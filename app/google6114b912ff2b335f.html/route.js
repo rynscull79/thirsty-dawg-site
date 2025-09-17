99a4f8808911cd7ad0dea6d074cc76911e888a2a
rynscull79 <tuckerandharper@gmail.com>
2025-09-17 00:11:44 -0500

    Taking forever

--- app/google6114b912ff2b335f.html/route.js
+++
@@ -1,7 +0,0 @@
-// app/google6114b912ff2b335f.html/route.js
-export async function GET() {
-  return new Response(
-    'google-site-verification: google6114b912ff2b335f.html',
-    { headers: { 'Content-Type': 'text/plain; charset=utf-8' } }
-  );
-}
