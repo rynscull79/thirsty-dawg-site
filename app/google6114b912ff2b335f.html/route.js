4244a3fdbb5209ae95ce2029ca4cfc8610c452e9
rynscull79 <tuckerandharper@gmail.com>
2025-09-17 00:09:35 -0500

    maybe this works

---
+++ app/google6114b912ff2b335f.html/route.js
@@ -0,0 +1,7 @@
+// app/google6114b912ff2b335f.html/route.js
+export async function GET() {
+  return new Response(
+    'google-site-verification: google6114b912ff2b335f.html',
+    { headers: { 'Content-Type': 'text/plain; charset=utf-8' } }
+  );
+}
