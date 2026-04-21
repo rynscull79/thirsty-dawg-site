56f97b2b49770e3a929353dcf7cd39bfd0765331
rynscull79 <tuckerandharper@gmail.com>
2026-04-20 20:25:50 -0500

    lazy load

--- src/app/layout.js
+++ src/app/layout.js
@@ -40,9 +40,9 @@ export default function RootLayout({ children }) {
       <head>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-6R94KFTC67"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="lazyOnload">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
